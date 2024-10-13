7babb9d822ebe18105f1e47f0728dc97c6166ade
Ankit Dutta <ankit.dutta0@gmail.com>
2024-10-14 00:26:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+from IPython.core.display import HTML
+
+# os.chdir("D:\\Project\\PycharmProjects\\Gen AI\\")
+HTML(r"""
+<style>
+    * {
+        #color: red;
+        #font-family: ‘Cascadia Code PL’;#,‘Courier New’, Courier, monospace; 
+        font-family: ‘Courier New’, Courier, monospace; 
+        font-size: 12px !important;
+        line-height: 1.1 !important;
+    }
+    .output-plaintext, .output-stream, .output {
+        font-family: ‘Courier New’, Courier, monospace; # Any monospaced font should work
+        line-height: 4 !important;
+        font-size: 34px !important;
+    }
+</style>
+""")
